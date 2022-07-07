7b93538112b0f219d2d2c6b43f32a19fb9da1edb
Quang Anh Nguyen <pipikopu@gmail.com>
2022-07-07 14:23:10 +0700

    Modify Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+
+    //inside class
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+
     public Transform playerTransform;
     public Transform modelTransform;
-    public Transform stack;
+    public GameObject yellowFlatPrefabs;
 
     private Vector3 targetPosition;
     private Vector3 startPosition;
@@ -25,52 +32,34 @@ public class Player : MonoBehaviour
     private Direction direction = Direction.Idle;
     private Direction nextDirection = Direction.Idle;
 
+    private int numOfStacks = 0;
+    public GameObject stackHolder;
+    public float stackDistance;
 
-    //inside class
-    Vector2 firstPressPos;
-    Vector2 secondPressPos;
-    Vector2 currentSwipe;
+    private int score;
+    public Text scoreText;
 
+    public GameObject closedArk;
+    public GameObject openArk;
+
+    private bool isWinning;
+    public GameObject winParticlesObject;
+    public GameObject completeMenu;
 
     // Start is called before the first frame update
     void Start()
     {
+        score = 0;
+        isWinning = false;
         direction = Direction.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (direction == Direction.Idle)
+        if (direction == Direction.Idle && !isWinning)
         {
             Swipe();
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                if (GetTargetPosition(Vector3.forward)) {
-                    direction = Direction.Forward;
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-                if (GetTargetPosition(Vector3.back))
-                {
-                    direction = Direction.Backward;
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.D))
-            {
-                if (GetTargetPosition(Vector3.right))
-                {
-                    direction = Direction.Right;
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.A))
-            {
-                if (GetTargetPosition(-Vector3.right))
-                {
-                    direction = Direction.Left;
-                }
-            }
         }
         else
         {
@@ -102,7 +91,7 @@ public class Player : MonoBehaviour
     private bool GetTargetPosition(Vector3 directionVec)
     {
         int layer_mask = LayerMask.GetMask("Wall");
-        if (Physics.Raycast(playerTransform.position - Vector3.up * 0.5f, playerTransform.TransformDirection(directionVec), out RaycastHit hit, Mathf.Infinity, layer_mask))
+        if (Physics.Raycast(playerTransform.position - Vector3.up * 0.5f, directionVec, out RaycastHit hit, Mathf.Infinity, layer_mask))
         {
             targetPosition = playerTransform.position + directionVec * (hit.distance - 0.5f);
             Debug.Log("Hit Wall");
@@ -113,75 +102,21 @@ public class Player : MonoBehaviour
         return false;
     }
 
-
-    public void SetNextDirection(int dir)
-    {
-        if (dir == 0) //DownRight
-        {
-            if (direction == Direction.Forward)
-            {
-                nextDirection = Direction.Right;
-            }
-            else if (direction == Direction.Left)
-            {
-                nextDirection = Direction.Backward;
-            }
-        }
-        else if (dir == 1) //UpRight
-        {
-            if (direction == Direction.Backward)
-            {
-                nextDirection = Direction.Right;
-            }
-            else if (direction == Direction.Left)
-            {
-                nextDirection = Direction.Forward;
-            }
-        }
-        else if(dir == 2) //DownLeft
-        {
-            if (direction == Direction.Right)
-            {
-                nextDirection = Direction.Backward;
-            }
-            else if (direction == Direction.Forward)
-            {
-                nextDirection = Direction.Left;
-            }
-        }
-        else if(dir == 3) //UpLeft
-        {
-            if (direction == Direction.Right)
-            {
-                nextDirection = Direction.Forward;
-            }
-            else if (direction == Direction.Backward)
-            {
-                nextDirection = Direction.Left;
-            }
-        }
-    }
-
     void Swipe()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //save began touch 2d point
             firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            //save ended touch 2d point
             secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-            //create vector from the two points
             currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
 
-            //normalize the 2d vector
             currentSwipe.Normalize();
 
-            //swipe upwards
             if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
             {
                 Debug.Log("up swipe");
@@ -190,9 +125,10 @@ public class Player : MonoBehaviour
                     direction = Direction.Forward;
                 }
             }
+
             //swipe down
             if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
-        {
+            {
                 Debug.Log("down swipe");
                 if (GetTargetPosition(Vector3.back))
                 {
@@ -201,7 +137,7 @@ public class Player : MonoBehaviour
             }
             //swipe left
             if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
-        {
+            {
                 Debug.Log("left swipe");
                 if (GetTargetPosition(-Vector3.right))
                 {
@@ -210,7 +146,7 @@ public class Player : MonoBehaviour
             }
             //swipe right
             if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
-        {
+            {
                 Debug.Log("right swipe");
                 if (GetTargetPosition(Vector3.right))
                 {
@@ -218,5 +154,110 @@ public class Player : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject stack = other.gameObject;
+        switch (stack.tag)
+        {
+            case "DownRight":
+                if (direction == Direction.Forward)
+                {
+                    nextDirection = Direction.Right;
+                }
+                else if (direction == Direction.Left)
+                {
+                    nextDirection = Direction.Backward;
+                }
+                break;
+
+            case "UpRight":
+                if (direction == Direction.Backward)
+                {
+                    nextDirection = Direction.Right;
+                }
+                else if (direction == Direction.Left)
+                {
+                    nextDirection = Direction.Forward;
+                }
+                break;
+
+            case "DownLeft":
+                if (direction == Direction.Right)
+                {
+                    nextDirection = Direction.Backward;
+                }
+                else if (direction == Direction.Forward)
+                {
+                    nextDirection = Direction.Left;
+                }
+                break;
+
+            case "UpLeft":
+                if (direction == Direction.Right)
+                {
+                    nextDirection = Direction.Forward;
+                }
+                else if (direction == Direction.Backward)
+                {
+                    nextDirection = Direction.Left;
+                }
+                break;
+
+            case "Stack":
+                //CollectStack();
+                if (numOfStacks != 0)
+                {
+                    modelTransform.position += Vector3.up * 0.4f;
+                    stackHolder.transform.position += Vector3.up * 0.4f;
+                }
+                stack.transform.SetParent(stackHolder.transform);
+                Debug.Log("Hit Stack");
+                stack.transform.position = modelTransform.position + Vector3.up * (-0.5f + numOfStacks * -stackDistance);
+                numOfStacks += 1;
+                score += 1;
+                scoreText.text = score.ToString();
+                stack.tag = "Untagged";
+                break;
+
+            case "BridgeBlock":
+                modelTransform.position -= Vector3.up * 0.4f;
+                numOfStacks -= 1;
+                Destroy(stackHolder.transform.GetChild(0).gameObject);
+                GameObject newYellowFlat = Instantiate(yellowFlatPrefabs) as GameObject;
+                stack.tag = "Untagged";
+                newYellowFlat.transform.SetParent(stack.transform);
+                newYellowFlat.transform.position = stack.transform.position + new Vector3(0, 0.02f, 0);
+                break;
+            case "WinBlock":
+                Debug.Log("Win Block");
+                stack.tag = "Untagged";
+                isWinning = true;
+                foreach (Transform child in stackHolder.transform)
+                {
+                    GameObject.Destroy(child.gameObject);
+                    modelTransform.position -= Vector3.up * 0.4f;
+                }
+                modelTransform.position += Vector3.up * 0.4f;
+                modelTransform.eulerAngles = Vector3.up * 0;
+                foreach (Transform child in winParticlesObject.transform)
+                {
+                    child.GetComponent<ParticleSystem>().Play();
+                }
+
+                modelTransform.gameObject.GetComponent<Animator>().Play("Take 2");
+                closedArk.SetActive(false);
+                openArk.SetActive(true);
+                StartCoroutine(completeGameDelay());
+                break;
+        }
+    }
+
+    IEnumerator completeGameDelay()
+    {
+        yield return new WaitForSeconds(2.5f);
+        completeMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 }
